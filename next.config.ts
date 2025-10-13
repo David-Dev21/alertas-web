ba232f23c34568576afe8f6adba2d03fdda02876
Ruben David <rubendavid.rd21@gmail.com>
2025-10-13 11:13:23 -0400

    Actualizacion next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Deshabilitar React StrictMode para testing
   reactStrictMode: false,
+  // Permitir orígenes específicos para solicitudes de desarrollo
+  allowedDevOrigins: ["jupiter-guardian.policia.bo"],
 };
 
 export default nextConfig;
